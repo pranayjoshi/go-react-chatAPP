698d691afe288ba64372ae3dcdf35cccbea2a7f3
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-18 02:25:28 -0600

    merged backend and front end

--- ui/src/layout/chat.jsx
+++ ui/src/layout/chat.jsx
@@ -15,12 +15,12 @@ export default function Chat() {
       console.log(messages);
     });
 }); // Add this line
-  function send(event) {
-    if (event.keyCode === 13) {
-      sendMsg(event.target.value);
-      event.target.value = "";
+
+const [message, setMessage] = useState("");
+  function send() {
+      sendMsg(message);
+      setMessage("");
     }
-  }
   return (
     <div className=" w-full">
       <main className="container mx-auto p-6 bg-gray-100">
@@ -76,12 +76,12 @@ export default function Chat() {
     <span className="relative flex shrink-0 overflow-hidden w-10 h-10 rounded-full"></span>
     <div>
       <div className="flex items-center space-x-2">
-        <h3 className="font-bold text-indigo-600">{msg.user}</h3>
+        <h3 className="font-bold text-indigo-600">{msg.data}</h3>
         <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-blue-200 text-blue-800">
-          {msg.time}
+          {msg.timeStamp}
         </div>
       </div>
-      <p className="text-gray-700">{msg.text}</p>
+      <p className="text-gray-700">{msg.data.body}</p>
     </div>
   </div>
 ))}
@@ -92,9 +92,9 @@ export default function Chat() {
                 className="flex h-10 w-full border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                 placeholder="Type your message here..."
                 type="text"
-                onClick={(event) => send(event)}
+                onChange={(event) => {setMessage(event.target.value);}}
               />
-              <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white">
+              <button onClick={(event) => send()} className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white">
                 Send
               </button>
             </div>
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
