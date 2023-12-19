1a265e734b8a5fda328f14ad2b6a28d09588040b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-19 12:35:35 -0600

    added

--- client/src/layout/chat.jsx
+++ client/src/layout/chat.jsx
@@ -9,18 +9,24 @@ import { connect, sendMsg } from "../api";
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
+  const [updates, setUpdates] = useState([]);
   useEffect(() => {
     connect((msg) => {
       console.log("New Message");
+      msg = JSON.parse(msg.data)
+      if (msg.type == "message") {
       setMessages((messages) => [...messages, msg]);
+      }
+      if (msg.type == "updates") {
+        setUpdates((updates) => [...updates, msg]);
+      }
       console.log(messages);
     });
   }); // Add this line
 
   const [message, setMessage] = useState("");
-  function returnMessage(message) {
-    const rmessage = JSON.parse(message.data);
-    return rmessage.body;
+  function returnMessage(message, index) {
+    return message[index];
   }
   function send() {
     sendMsg(message);
@@ -42,53 +48,19 @@ export default function Chat() {
                 className="h-full w-full rounded-[inherit]"
                 //   style="overflow: hidden scroll;"
               >
-                <div className="min-w-full table ">
-                  <div className="space-y-4">
-                    <div className="flex items-start space-x-4">
-                      <span className="relative flex shrink-0 overflow-hidden w-10 h-10 rounded-full"></span>
-                      <div>
-                        <div className="flex items-center space-x-2">
-                          <h3 className="font-bold text-indigo-600">
-                            User Name
-                          </h3>
-                          <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-blue-200 text-blue-800">
-                            10:00 AM
-                          </div>
-                        </div>
-                        <p className="text-gray-700">Hello! How are you?</p>
-                      </div>
-                    </div>
-                    <div className="flex items-start space-x-4">
-                      <span className="relative flex shrink-0 overflow-hidden w-10 h-10 rounded-full"></span>
-                      <div>
-                        <div className="flex items-center space-x-2">
-                          <h3 className="font-bold text-indigo-600">
-                            User Name
-                          </h3>
-                          <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-blue-200 text-blue-800">
-                            10:05 AM
-                          </div>
-                        </div>
-                        <p className="text-gray-700">
-                          I'm doing great, thank you!
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 {messages.map((msg, index) => (
                   <div key={index} className="flex items-start space-x-4">
                     <span className="relative flex shrink-0 overflow-hidden w-10 h-10 rounded-full"></span>
                     <div>
                       <div className="flex items-center space-x-2">
                         <h3 className="font-bold text-indigo-600">
-                          {msg.data}
+                        {returnMessage(msg, "user")}
                         </h3>
                         <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-blue-200 text-blue-800">
                           {Date(msg.timeStamp * 1000)}
                         </div>
                       </div>
-                      <p className="text-gray-700">{returnMessage(msg)}</p>
+                      <p className="text-gray-700">{returnMessage(msg, "body")}</p>
                     </div>
                   </div>
                 ))}
