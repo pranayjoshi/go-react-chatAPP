d402c9e895b5a93e09c2cac216b3ce4e5d98c0e1
pranayjoshi <pranayjoshi4466@gmail.com>
2023-12-20 00:05:11 -0600

    added

--- client/src/layout/chat.jsx
+++ client/src/layout/chat.jsx
@@ -6,7 +6,6 @@
 import { useEffect, useState } from "react";
 import { connect, sendMsg, initializeSocket } from "../api";
 
-
 export default function Chat() {
   const [messages, setMessages] = useState([]);
   const [updates, setUpdates] = useState([]);
@@ -14,15 +13,17 @@ export default function Chat() {
   useEffect(() => {
     initializeSocket();
     connect((msg) => {
-      msg = JSON.parse(msg.data)
+      msg = JSON.parse(msg.data);
       if (msg.type == "message") {
         setMessages((messages) => [...messages, msg]);
       }
       if (msg.type == "updates") {
         setUpdates((updates) => [...updates, msg]);
         console.log(updates);
-        if (msg.body == "joined") {
+        if (msg.body == "New User Joined...") {
           setUsers((users) => [...users, msg.user]);
+        } else {
+          setUsers((users) => users.filter((user) => user != msg.user));
         }
       }
       console.log(messages);
@@ -59,13 +60,15 @@ export default function Chat() {
                     <div>
                       <div className="flex items-center space-x-2">
                         <h3 className="font-bold text-indigo-600">
-                        {returnMessage(msg, "user")}
+                          {returnMessage(msg, "user")}
                         </h3>
                         <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent hover:bg-primary/80 bg-blue-200 text-blue-800">
                           {Date(msg.timeStamp * 1000)}
                         </div>
                       </div>
-                      <p className="text-gray-700">{returnMessage(msg, "body")}</p>
+                      <p className="text-gray-700">
+                        {returnMessage(msg, "body")}
+                      </p>
                     </div>
                   </div>
                 ))}
@@ -118,6 +121,28 @@ export default function Chat() {
                     </div>
                   </li>
                 </ul>
+                <div
+                className="h-full w-full rounded-[inherit]"
+                //   style="overflow: hidden scroll;"
+              >
+                <div
+                  dir="ltr"
+                  className="relative overflow-hidden h-96 mb-4 bg-gray-200 p-4 rounded-md overflow-y-auto mx-4"
+                  // ="position: relative; --radix-scroll-area-corner-width: 0px; --radix-scroll-area-corner-height: 0px;"
+                >
+                  {users.map((msg, index) => (
+                    <div key={index} className="flex items-start space-x-4">
+                      <div>
+                        <div className="flex items-center space-x-2">
+                          <h3 className="font-bold text-indigo-600">
+                            {msg}
+                          </h3>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
               </div>
             </div>
             <div
@@ -134,24 +159,26 @@ export default function Chat() {
                 //   style="overflow: hidden scroll;"
               >
                 <div
-              dir="ltr"
-              className="relative overflow-hidden h-96 mb-4 bg-gray-200 p-4 rounded-md overflow-y-auto mx-4"
-              // ="position: relative; --radix-scroll-area-corner-width: 0px; --radix-scroll-area-corner-height: 0px;"
-            >
-                {updates.map((msg, index) => (
-                  <div key={index} className="flex items-start space-x-4">
-                    <div>
-                      <div className="flex items-center space-x-2">
-                        <h3 className="font-bold text-indigo-600">
-                        {returnMessage(msg, "body")}
-                        </h3>
+                  dir="ltr"
+                  className="relative overflow-hidden h-96 mb-4 bg-gray-200 p-4 rounded-md overflow-y-auto mx-4"
+                  // ="position: relative; --radix-scroll-area-corner-width: 0px; --radix-scroll-area-corner-height: 0px;"
+                >
+                  {updates.map((msg, index) => (
+                    <div key={index} className="flex items-start space-x-4">
+                      <div>
+                        <div className="flex items-center space-x-2">
+                          <h3 className="font-bold text-indigo-600">
+                            {returnMessage(msg, "body")}
+                          </h3>
+                        </div>
+                        <p className="text-gray-700">
+                          {returnMessage(msg, "user")}
+                        </p>
                       </div>
-                      <p className="text-gray-700">{returnMessage(msg, "user")}</p>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
             </div>
           </div>
         </div>
